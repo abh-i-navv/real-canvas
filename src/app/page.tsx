3f6e6393c4b1a7f38f3c19dd428e649d46ab8a6d
abh-i-navv <abhinav11290@gmail.com>
2024-05-31 13:17:07 +0530

    cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,6 @@ import { Canvas } from "./components/canvas";
 
 export default function Home() {
 
-  const onPointerDown = () =>{
-    console.log(1)
-  }
-
-  const layer = { x: 100,
-    y:100,
-    width:200,
-    height:200}
-
   return (
     <main >
       <div className="h-full w-full flex justify-center items-center overflow-y-scroll no-scrollbar overflow-hidden">
